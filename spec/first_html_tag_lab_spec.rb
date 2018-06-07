02ddc4f8901bd053763e694f76fd31e346c02d8f
Peter Bell <PeterBell@users.noreply.github.com>
2018-06-07 16:53:42 -0600

    Making spec less brittle - include instead of eq
    to cover whitespace, etc

--- spec/first_html_tag_lab_spec.rb
+++ spec/first_html_tag_lab_spec.rb
@@ -8,6 +8,6 @@ RSpec.describe 'Your First HTML Tag' do
   it "the H1 contains 'Hello, World!'" do
     h1_content = parsed_html.search('h1').text
 
-    expect(h1_content).to eq('Hello, World!'), hint(2, h1_content)
+    expect(h1_content).to include('Hello, World!'), hint(2, h1_content)
   end
 end
