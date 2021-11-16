4ec47d82d26ebfad499cfd40a5f0fa6aa77ae3cc
ihollander <ianholla@gmail.com>
2021-11-16 09:25:01 -0500

    Added chai-dom

--- test/index.html.test.js
+++ test/index.html.test.js
@@ -1,20 +1,22 @@
-const { expect } = require("chai");
+const chai = require("chai");
+chai.use(require("chai-dom"));
+const { expect } = chai;
 
 describe("index.html", () => {
   it("has a <h1> element", () => {
     const h1 = document.querySelector("h1");
-    const errorHint = "Did you code an <h1>Tag</h1> in index.html?";
+    const hint = "Did you code an <h1>Tag</h1> in index.html?";
 
-    expect(h1, errorHint).to.exist;
+    expect(h1, hint).to.exist;
   });
 
   describe("the <h1> element", () => {
     it('contains the text "Hello, World!"', () => {
       const h1 = document.querySelector("h1");
-      const errorHint =
+      const hint =
         'Does your H1 have exactly "Hello, World!" inside the opening and closing tag?';
 
-      expect(h1.textContent, errorHint).to.contain("Hello, World!");
+      expect(h1, hint).to.have.text("Hello, World!");
     });
   });
 });
